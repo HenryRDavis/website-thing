c204f87c312e45fdcb29ba410e95c162ebc9eb30
HenryRDavis <rainfallsprings@gmail.com>
2021-04-22 18:28:58 -0500

    deleted everything auth related

--- api/server.js
+++ api/server.js
@@ -1,29 +1,24 @@
 const express = require('express')
+const cors = require('cors')
 const helmet = require('helmet')
+const userRouter = require('../users/user_router')
+const postRouter = require('../instructors/instructor_router')
+const server = express()
 
-const server = express();
 
-const sessionConfiguration = {
-  name: 'monster',
-  secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe!',
-  cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 10,
-      secure: process.env.SECURE_COOKIES || false,
-  },
-  resave: false,
-  saveUninitialized: true,
-}
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
-server.use(session(sessionConfiguration));
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
 
+ 
+
+// to test that the server is up and running
 server.get('/', (req, res) => {
-  res.status(200).json({message: 'Working'})
+    res.status(200).send(`<h1> Server is Up and Running </h1>`)
 })
-server.use('/api/auth', authRouter);
 
+server.use('/api/users', userRouter)
+server.use('/api/instructors', postRouter)
 
 module.exports = server
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config() // to allow the deployment server to access the env comands
+
 const server = require('./api/server.js')
 
 const port = process.env.PORT || 5000
